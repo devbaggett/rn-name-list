16413c233440dc8bbcaadd98552b5919ffaac534
devbaggett <devabaggett@gmail.com>
2021-01-19 20:27:56 -0800

    Added ImageScreen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,6 +11,9 @@ const HomeScreen = ({navigation}) => {
             <Button
                 onPress={() => navigation.navigate("List")}
                 title="Go to List Demo"/>
+          <Button
+              onPress={() => navigation.navigate("Image")}
+              title="Go to Image Demo"/>
         </View>
     );
 };
