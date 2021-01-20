36c0d814e91dd61daae3b92c91df2472b3b8e414
devbaggett <devabaggett@gmail.com>
2021-01-19 20:51:22 -0800

    Added CounterScreen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,9 +11,12 @@ const HomeScreen = ({navigation}) => {
             <Button
                 onPress={() => navigation.navigate("List")}
                 title="Go to List Demo"/>
-          <Button
-              onPress={() => navigation.navigate("Image")}
-              title="Go to Image Demo"/>
+            <Button
+                onPress={() => navigation.navigate("Image")}
+                title="Go to Image Demo"/>
+            <Button
+                onPress={() => navigation.navigate("Counter")}
+                title="Go to Counter Demo"/>
         </View>
     );
 };
