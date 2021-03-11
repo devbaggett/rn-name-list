f038b8b5543df11d6ce7b4ae260ce29fe2d8b905
devbaggett <devabaggett@gmail.com>
2021-03-11 15:12:55 -0800

    Added reducer to Square Demo.

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,53 @@
+import React, {useReducer} from "react";
+import {StyleSheet, View} from "react-native";
+import ColorCounter from "../components/ColorCounter";
+
+const COLOR_INCREMENT = 15;
+
+const reducer = (state, action) => {
+    const {red, green, blue} = state;
+    const {type, payload} = action;
+
+    switch (type) {
+        case "change_red":
+            return red + payload > 255 || red + payload < 0 ? state : {...state, red: red + payload};
+        case "change_green":
+            return green + payload > 255 || green + payload < 0 ? state : {...state, green: green + payload};
+        case "change_blue":
+            return blue + payload > 255 || blue + payload < 0 ? state : {...state, blue: blue + payload};
+        default:
+            return state;
+    }
+};
+
+const SquareScreen = () => {
+    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
+    const {red, green, blue} = state;
+
+    console.log(state);
+
+    return (
+        <View>
+            <ColorCounter onIncrease={() => dispatch({type: "change_red", payload: COLOR_INCREMENT})}
+                          onDecrease={() => dispatch({type: "change_red", payload: -1 * COLOR_INCREMENT})}
+                          color="Red" />
+            <ColorCounter onIncrease={() => dispatch({type: "change_green", payload: COLOR_INCREMENT})}
+                          onDecrease={() => dispatch({type: "change_green", payload: -1 * COLOR_INCREMENT})}
+                          color="Green" />
+            <ColorCounter onIncrease={() => dispatch({type: "change_blue", payload: COLOR_INCREMENT})}
+                          onDecrease={() => dispatch({type: "change_blue", payload: -1 * COLOR_INCREMENT})}
+                          color="Blue" />
+            <View
+                style={{
+                    height: 150,
+                    width: 150,
+                    backgroundColor: `rgb(${red}, ${green}, ${blue})`
+                }}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
